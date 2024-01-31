403976c9e6adf03d51eef6e77593931d97b1cfeb
Lavanya S N <108964781+LavanyaSN@users.noreply.github.com>
2024-01-31 15:34:11 +0530

    Add files via upload

---
+++ registrationpage/registrationpage/src/main/java/com/angularproject/registrationpage/Controller/controller.java
@@ -0,0 +1,28 @@
+package com.angularproject.registrationpage.Controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.angularproject.registrationpage.Model.registration;
+import com.angularproject.registrationpage.Service.studentservice;
+
+@RestController
+@CrossOrigin(origins="http://localhost:4200/",maxAge=3600)
+
+@RequestMapping("/student")
+public class controller  {
+    
+    @Autowired
+    
+    studentservice studentserv;
+
+    @PostMapping("/registration")
+    public registration  addData(@RequestBody registration registration){
+        return this.studentserv.addStudentService(registration);
+    }
+
+}
